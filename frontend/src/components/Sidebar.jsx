f7f2c68efbdc8aff665f0cfda913d0fb7ea81c08
toaingo2108 <nqtoai2181@gmail.com>
2022-04-21 11:38:04 +0700

    Push full code

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import { RiHomeFill } from 'react-icons/ri'
+// import { IoIosArrowForward } from 'react-icons/io'
+
+import logo from '../assets/logo.png'
+
+import { categories } from '../utils/data'
+// const categories = [
+//     { name: 'Animals' },
+//     { name: 'Wallapapers' },
+//     { name: 'Photography' },
+//     { name: 'Gaming' },
+//     { name: 'Coding' },
+//     { name: 'Other' },
+// ]
+
+const isActiveStyle =
+    'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black text-gray-500 transition-all duration-200 easein-out capitalize'
+const isNotActiveStyle =
+    'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 easein-out capitalize'
+
+const Sidebar = ({ user, closeToggle }) => {
+    const handleCloseSidebar = () => {
+        if (closeToggle) closeToggle(false)
+    }
+
+    return (
+        <div className="flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar">
+            <div className="flex flex-col">
+                <Link
+                    to="/"
+                    className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+                    onClick={handleCloseSidebar}>
+                    <img src={logo} alt="logo" className="w-full" />
+                </Link>
+                <div className="flex flex-col gap-5">
+                    <NavLink
+                        to="/"
+                        // prop: isActive 'react-router-dom'
+                        className={({ isActive }) =>
+                            isActive ? isActiveStyle : isNotActiveStyle
+                        }
+                        onClick={handleCloseSidebar}>
+                        <RiHomeFill />
+                        Home
+                    </NavLink>
+                    <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+                        Discover categories
+                    </h3>
+                    {categories
+                        .slice(0, categories.length - 1)
+                        .map((category) => (
+                            <NavLink
+                                key={category.name}
+                                to={`/category/${category.name}`}
+                                // prop: isActive 'react-router-dom'
+                                className={({ isActive }) =>
+                                    isActive ? isActiveStyle : isNotActiveStyle
+                                }
+                                onClick={handleCloseSidebar}>
+                                <img
+                                    src={category.image}
+                                    alt="category-pic"
+                                    className="w-8 h-8 rounded-full shadow-sm"
+                                />
+                                {category.name}
+                            </NavLink>
+                        ))}
+                </div>
+            </div>
+            {user && (
+                <Link
+                    to={`user-profile/${user?._id}`}
+                    className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+                    onClick={handleCloseSidebar}>
+                    <img
+                        src={user.image}
+                        alt="user-profile"
+                        className="w-10 h-10 rounded-full"
+                    />
+                    <p>{user.userName}</p>
+                </Link>
+            )}
+        </div>
+    )
+}
+
+export default Sidebar
