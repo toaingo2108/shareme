5536fd174d5a4b75c116d6fddbcf27d852d166f8
toaingo2108 <nqtoai2181@gmail.com>
2022-04-23 14:02:40 +0700

    Edit somethings

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { IoMdSearch } from 'react-icons/io'
 
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
     const navigate = useNavigate()
